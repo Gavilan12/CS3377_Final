3922c0159104dcca50d0bf6db5a2f96b6fc67bcf
Gavilan12 <43182579+Gavilan12@users.noreply.github.com>
2018-12-16 01:59:49 -0600

    problem 1 bash done

---
+++ Prob1Bash.sh
@@ -0,0 +1,18 @@
+i=1
+PointsInSquare=$(($1))
+PointsInQuarterCircle=0
+for i in $(seq $PointsInSquare); do
+read x y < <(awk -v seed=$RANDOM 'BEGIN{srand(seed);print rand(), rand()}')
+X=$(bc <<< "scale=7; $x*$x")
+Y=$(bc <<< "scale=7; $y*$y")
+XY=$(bc <<< "scale=7; $X+$Y")
+if(( $(echo "$XY<=1.000" |bc -l) )); then
+((PointsInQuarterCircle++))
+fi
+((i++))
+done
+MyPi=$(bc <<< " scale=5; (($PointsInQuarterCircle / $PointsInSquare) * 4 )/1  ")
+echo "PointsInQuarterCircle $PointsInQuarterCircle : PointsInSquare $PointsInSquare"
+echo "Pi = $MYPi"
+
+
